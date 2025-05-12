50a23177e7699b4891cf4fde036d684708b3bb15
Bruno Ferreira <134228992+Brunooo07@users.noreply.github.com>
2025-05-12 16:25:54 -0300

    MVC-LOGIN
    MVC BÁSICO PARA LOGIN E BANCO WAMP

---
+++ Controller.py
@@ -0,0 +1,45 @@
+from Model import Pessoa
+from Model import Bancoatributos
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import hashlib
+
+def retorna_session():
+    a = Bancoatributos()
+    CONN = f"mysql+pymysql://{a.USUARIO}:{a.SENHA}@{a.HOST}:{a.PORT}/{a.BANCO}"
+    engine = create_engine(CONN, echo = True)
+    Session = sessionmaker(bind=engine)
+    return Session()
+
+class ControllerCadastro():
+    @classmethod
+    def verifica_dados(cls, nome, email, senha):
+        if len(nome) > 50 or len(nome) < 3:
+            return 2
+        if len(email) > 200:
+            return 3
+        if len(senha) > 100 or len(senha) < 6:
+            return 4 
+        return 1
+    
+    @classmethod
+    def cadastrar(cls, nome, email, senha):
+        session = retorna_session()
+        usuario = session.query(Pessoa).filter(Pessoa.email == email).all()
+        if len(usuario) > 0:
+            return 5 
+        dados_verificados = cls.verifica_dados(nome, email, senha)
+        if dados_verificados != 1:
+            return dados_verificados
+        try:
+            senha = hashlib.sha256(senha.encode())
+            p1 = Pessoa(nome=nome, email=email, senha=senha)
+            session.add(p1)
+            session.commit()
+            return 1
+        except:
+            return 3
+senha = "minha senha"
+print(hashlib.sha256(senha.encode()).hexdigest()) 
+
+ControllerCadastro.cadastrar('Caio','brunosilva707092@gmail.com', 'bruno123@123')
---
+++ Model.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+class Bancoatributos:
+    CONN = f"mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORT}/{BANCO}"
+a = Bancoatributos()
+
+engine = create_engine(a.CONN, echo= True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Pessoa(Base):
+    __tablename__ = "Pessoa"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(50))
+    email = Column(String(200))
+    senha = Column(String(100))
+    
+Base.metadata.create_all(engine)
+ 
